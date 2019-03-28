ef282b280196a4fa059c7b251e3138cbcc4ec8c5
Stephen <Stephen@Stephen-PC>
2019-03-27 18:53:37 -0700

    Sample source. Greeting team.

--- IPASS/src/JavaClass.java
+++ IPASS/src/JavaClass.java
@@ -1,4 +1,12 @@
 
 public class JavaClass {
 
+	public static void main(String[] args) {
+		
+		String[] names = { "Stephen", "Swabinash", "Austin", "Israel", "Pezhman"};
+		
+		for ( int i = 0; i < names.length; i++) {
+			System.out.format("Hello, %s!\n", names[i]);
+		}
+	}
 }
