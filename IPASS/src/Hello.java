7aceaeef493660423d48ee1d435e43382f85307d
Austin <Austin@192.168.1.199>
2019-04-04 22:40:31 -0700

    first commit

--- IPASS/src/Hello.java
+++ IPASS/src/Hello.java
@@ -1,16 +1,16 @@
 public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Git!");
-		System.out.println("yang - This line was added by Dr. Yang.");
-		// Add a new line with your informationSystem.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("211647268 - This line was added by Stephen Renda.");
-		System.out.println("219268492 - This line was added by Swabinash Swarnaraja.");
-		System.out.println("219994724 - This line was added by Israel Jimenez.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git!");
+        System.out.println("yang - This line was added by Dr. Yang.");
+        // Add a new line with your informationSystem.out.println("SacStateID - This line was added by First name Last name.");
+        System.out.println("211647268 - This line was added by Stephen Renda.");
+        System.out.println("219268492 - This line was added by Swabinash Swarnaraja.");
+        System.out.println("219994724 - This line was added by Israel Jimenez.");
+        System.out.println("SacStateID - This line was added by Austin Heritage.");
+        System.out.println("SacStateID - This line was added by First name Last name.");
+        System.out.println("SacStateID - This line was added by First name Last name.");
+        System.out.println("SacStateID - This line was added by First name Last name.");
+        System.out.println("SacStateID - This line was added by First name Last name.");
+        System.out.println("SacStateID - This line was added by First name Last name.");
+    }
 }
