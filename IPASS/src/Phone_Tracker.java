b51d788191da096c2cbdcac06a8b8b85e74f0d95
Stephen <Stephen@Stephen-PC>
2019-04-08 12:15:31 -0700

    Create code that reads/opens Phone_DataBase.txt from a file. From Card #1 /First task

---
+++ IPASS/src/Phone_Tracker.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Phone_Tracker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        BufferedReader reader;
+        List<String> records = new ArrayList<String>();
+        try {
+        	reader = new BufferedReader(new FileReader("/Users/Public/myfile.txt"));
+        	/* File has format like this
+        	   Stephen 1 N street
+			   John 0 Blank
+        	*/
+        	String line = reader.readLine();
+        	records.add(line);
+        	while (line != null) {
+        		//System.out.println(line);
+        		line = reader.readLine();
+        		records.add(line);
+        	}
+        	for (int i= 0; i < records.size()-1; i++)
+        	{
+        		System.out.println(records.get(i));
+        	}
+        	reader.close();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+	}
+}
