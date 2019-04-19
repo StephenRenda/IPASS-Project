8df2f02849f538171822ea71b1d71a51ca9c5b9b
Stephen <Stephen@Stephen-PC>
2019-04-18 20:28:29 -0700

    Still needs work!
    Server Data is read into Person class. Person then selects "Phone
    missing". Person gets Finder Phone data and updates location on server.

---
+++ IPASS/src/PhoneTracker/Person.java
@@ -0,0 +1,67 @@
+package PhoneTracker;
+import java.io.Serializable;
+
+public class Person implements Serializable	{
+	
+		
+		private static final long serialVersionUID = 1L;
+		
+		public String first_name;
+		public String last_name;
+		public String isLost;
+		public String location;
+		public Person(String line)
+		{
+			String[] split = line.split(",");
+			first_name = split[0];
+			last_name = split[1];
+			isLost = split[2];
+			location = split[3];
+		}
+	
+		
+		public Person(String fname, String lname, String lost, String location)
+		{
+			this.first_name = fname;
+			this.last_name = lname;
+			this.isLost = lost;
+			this.location = location;
+		}
+
+
+		public void setFirstName(String fname) {
+	        this.first_name = fname;
+	    }
+	 
+	    public String getFirstName() {
+	        return this.first_name;
+	    }
+	 
+	    public void setLastName(String lname) {
+	        this.first_name = lname;
+	    }
+	 
+	    public String getLastName() {
+	        return this.last_name;
+	    }
+	 
+	    public void setLost(String lost) {
+	        this.isLost = lost;
+	    }
+	 
+	    public String getLost() {
+	        return this.isLost;
+	    }
+	    public void setLocation(String location) {
+	        this.location = location;
+	    }
+	 
+	    public String getLocation() {
+	        return this.location;
+	    }
+        @Override
+        public String toString() {
+            return this.first_name + "," + this.last_name + "," +
+            	   this.isLost + "," + this.location;
+        }        
+	}
---
+++ IPASS/src/PhoneTracker/Phone_Tracker.java
@@ -0,0 +1,113 @@
+package PhoneTracker;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Phone_Tracker {
+	
+	private static Scanner scanner;
+	static String Server = "/Users/Public/Phone_DataBase.txt";
+	static String Finder = "/Users/Public/locations.txt";
+	public static void main(String arg[]) {
+		
+        ReadFile rf = new ReadFile();
+        HashMap<String, String> map = new HashMap<String, String>();
+        
+        scanner = new Scanner(System.in);
+
+        //We can register a new User here.
+        
+        //For now Just lookup by name.
+        System.out.print("What is your name: \n");
+        String name = scanner.nextLine();
+   
+        try {//Putting location data from Finder into a hash map.
+			BufferedReader reader = new BufferedReader(new FileReader(Finder));
+        	String line;
+        	int index;
+			while ((line = reader.readLine()) != null)
+            {//Puts Finder Data into a hash map
+                String[] parts = line.split(",", 2);
+                if (parts.length >= 2)
+                {
+                    String key = parts[0];
+                    String value = parts[1];
+                    map.put(key, value);
+                } else {
+                    System.out.println("ignoring line: " + line);
+                }
+            } 
+			reader.close();
+			//Putting Server data into an array before person object
+        	String[] lines = rf.readLines(Server);   
+        	
+        	Person[] person = new Person[lines.length];
+        	for (int i = 0; i < lines.length-1; i++)
+        	{//Filling person object array with Server data.
+        		person[i] = new Person(lines[i]); 
+        	}
+        	//Set "phone is lost" by changing lost to 1.
+        	
+			
+ 
+			for (index = 0; index < person.length-1;index++)
+			{// Finds index, so we know what line to replace
+				if (name.contentEquals(person[index].getFirstName()))
+					break; 
+			}
+			
+			//Save old line.
+			String tempLine = person[index].toString();
+			
+			//Update person with location.
+			person[index].setLocation(map.get(name));
+
+			
+	        ModifyFile mf = new ModifyFile();
+	        //Copy person data to Server.
+            mf.modifyFile(Server, tempLine, person[index].toString());
+
+        	} catch (IOException e) {
+        	e.printStackTrace();
+        	}
+        phoneIsLost(map,name);
+         // call to phoneIsLost()  
+    }	
+	
+	private static void phoneIsLost(HashMap<String, String> map, String name) {
+        System.out.println("Is you phone lost?:");
+        String c;
+        Scanner S=new Scanner(System.in);
+        c= S.next();
+        if (c.contentEquals("yes"))
+        	for(int i=0;i<3;i++)
+        	{
+        		try {
+        			Thread.sleep(500);
+        	        System.out.println("Locating phone......");
+        		} catch (InterruptedException e) {
+        			e.printStackTrace();
+        		}
+        	}
+
+        System.out.println("Phone location:"+ map.get(name));
+        S.close();
+	}
+
+/*	private static void updatePhoneIsLost(String name, String address) {
+		
+        try (FileWriter writer = new FileWriter("/Users/Public/Phone_DataBase.txt", true);
+        	BufferedWriter bw = new BufferedWriter(writer)) {
+        	System. out. print(System.lineSeparator());
+        	bw.write(name + "\t\t" + address);
+        	bw.newLine();
+        } 
+        catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+	}
+*/
+}
